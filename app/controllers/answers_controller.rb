853b9381a1964763f08388f61ec0a95f72c46cca
Patricia Carbajal <carbajal.patricia@gmail.com>
2015-01-31 12:44:22 -0500

    add links to add new answer and view all answers for a particular question

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,21 +1,24 @@
 class AnswersController < ApplicationController
 
   def index
-    @answers = Answer.all
+    @answers = Answer.where(question_id: params[:question] )
   end
 
   def new
     @answer = Answer.new
+    @question = Question.find(params[:format])
+    p "**************"
+    p @question
   end
 
   def create
     @answer = Answer.create(post_params)
+    @question = Question.find(@answer.question_id)
     render :show
   end
 
   def show
     @answer = Answer.find(post_params)
-    @question = Question.find(@answer.question_id)
   end
 
   def edit
@@ -28,3 +31,28 @@ class AnswersController < ApplicationController
     params.require(:answer).permit(:code, :prose, :question_id)
   end
 end
+
+
+# public Class StackWithMin extends Stack < NodeWithMin> {
+# int newMin = Math.min(value, min());
+# super.push(newNodeWithMin(value, newMin));
+# }
+
+# public int min() {
+#   if (this.isEmpty()) {
+#     return Integer.MAX_VALUE. }
+# else {
+# return peek().min;
+# }
+# }
+# }
+# class NodeWithMin {
+# public int value;
+# public int min;
+# public NodeWithMin {
+# public int value;
+# public NodeWithMin(int v, int min){
+# value = v;
+# this.min = min;
+# }
+# }
