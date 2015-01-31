ac5da602425d409ca455c0a107132852aeef09fe
Luke Thomas <lukert33@gmail.com>
2015-01-31 12:31:02 -0500

    Users; new and create db cycle.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,25 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.create(user_params)
+    redirect_to root_path
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(params[:user])
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:email, :username, :password, :password_confirmation, :cohort)
+  end
+
 end
